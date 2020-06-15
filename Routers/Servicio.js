f821980798e3672312360b8c6588104417a437c5
Holiber Rodriguez <hfrodriguezs@poligran.edu.co>
2020-06-15 15:43:35 -0500

    Version 2.1 HR

--- Routers/Servicio.js
+++ Routers/Servicio.js
@@ -1,5 +1,6 @@
 const sql = require('mssql');
 const express = require('express');
+const { request } = require('https');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -16,15 +17,30 @@ router.post('/crearServicio', (req, res) => {
     request.input('NoServicio', req.body.noServicio)
     request.input('FechaServicio', req.body.fechaServicio)
  
-    request.execute('CrearProducto', function (err, result) {
+    request.execute('CrearServicio', function (err, result) {
         if (err) console.log(err)
         res.send(result.rowsAffected);
     });
 
 });
-router.put('path', (req, res) => {
+router.put('/actualizarServicio', (req, res) => {
+    request.input('IdServicio', req.body.idServicio)
+    request.input('IdSuc', req.body.idSuc)
+    request.input('IdEstado', req.body.idEstado)
+    request.input('PlacaServicio', req.body.placaServicio)
+    request.input('NoServicio', req.body.noServicio)
+    request.input('FechaServicio', req.body.fechaServicio)
+    
+        request.execute('ActualizarServicio', function (err, result) {
+            if (err) console.log(err)
+            res.send(result.rowsAffected);
+        });
+
 
 });
-router.delete('path', (req, res) => {
-
+router.delete('/eliminar', (req, res) => {
+    request.query(' DELETE FROM Sucursal WHERE IdSuc = '+ req.body.idSuc, function (err, result) {
+        if (err) console.log(err)
+        res.send(result.rowsAffected);
+    });
 });
